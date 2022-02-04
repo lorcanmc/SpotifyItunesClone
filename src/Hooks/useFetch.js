ec46842ace277e57b5fc60fffa9961f74534b813
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2022-02-04 15:26:08 +0000

    Added useFetch hook and gave unique keys to children

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,18 @@
+import react, { useState, useEffect } from "react";
+
+export default function useFetch(query) {
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const res = await fetch(
+        `https://itunes.apple.com/search?term=${query}&media=music&enitity=album&limit=25`
+      );
+      const json = await res.json();
+      setResponse(json.results);
+    }
+    loadData();
+  }, [query]);
+
+  return response;
+}
