51459d2f378b9d18c9bf75af6b8c9b10e20e0301
Lorcan Mc <lorcan.jmc@gmail.com>
2022-02-04 12:26:21 +0000

    trying useEffect

---
+++ plan.md
@@ -0,0 +1,19 @@
+Music Search APP
+
+Use itunes api: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+
+functionality:
+  MVP:
+    Header with name of app
+    Search by name
+    Call to itunes API (https://itunes.apple.com/search?term=jack+johnson&limit=25)
+    Display the 25 results, mapped down the page
+    Each track will have a artist name(artistName),trackname(trackName), album name(collectionName), image (artWorkUrl100 from the API)
+
+
+
+Components:
+    Header
+    Input for artist name
+    TrackList
+    Track
--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from "./soc-logo.svg";
 import "./App.css";
 
+import { useEffect } from "react";
+
 function App() {
+  // useEffect(() => {
+  //   const loadData = async function () {
+  //   const response = await fetch(API_URL);
+  //   const data = await response.json();
+  //   setRecipes(data);
+  //   console.log(data);
+  // };
+  //   loadData();
+  // }, [query]);
+
+  useEffect(() => {
+    const loadData = async function () {
+      const response = await fetch(
+        "https://itunes.apple.com/search?term=jack+johnson&limit=25", {
+          mode: 'no-cors',
+          method: "get",
+          headers: {
+                "Content-Type": "application/json"
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+    loadData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
